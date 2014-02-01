d7baaff425c92653f4e2ff2836f133ebb525b9ff
stanzheng <dev.stanzheng@gmail.com>
2014-02-01 14:49:21 -0500

    added dependencies

---
+++ project/include/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+<script src="bower_components/jquery/jquery.min.js" type="text/javascript" charset="utf-8" async defer></script>
+<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
+</head>
+
+
+
---
+++ project/index.php
@@ -0,0 +1,20 @@
+<?php include 'include/header.php';?>
+<?php include 'include/nav.php';?>
+    <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+        <h1>Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a href="#" class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+      </div>
+<?php 
+for ($i=0; $i < 10; $i++) { 
+	# code...
+	echo '<p>Food Item'.$i.'</p>';
+}
+?>
+
+<?php include 'include/footer.php'; ?>
+</body>
+<p> built by stanley zheng and lookmai rattana </p>
+</html>
+
