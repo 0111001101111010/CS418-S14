4cbfd51986b1fe16bf30c06f987e0d8f9fd20a1a
Lookmai Rattana <oratt001@gmail.com>
2014-03-04 01:31:37 -0500

    thread

--- project/forum.php
+++ project/forum.php
@@ -69,8 +69,29 @@ else {
 
 
 ?>
+<button class="btn btn-blue pull-right" data-toggle="modal" data-target="#newboard" >New Thread <i class="fa fa-plus-square"></i></button>
 
+
+  <!-- Modal -->
+  <div class="modal fade" id="newboard" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title" id="myModalLabel">New Thread</h4>
+        </div>
+        <div class="modal-body">
+          <form>
+            <input type="text" placeholder="Enter Title"></input><br>
+            <textarea placeholder="Enter Description"></textarea>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <a href="#" class="btn btn-blue">Create</a>
+        </div>
+      </div>
+    </div>
+  </div>
 <?php include 'include/footer.php'; ?>
 </body>
-<p> built by stanley zheng and lookmai rattana </p>
 </html>
