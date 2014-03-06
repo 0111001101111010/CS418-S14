5c3393e2a836a9aaf056cc80ab730cd126804a3a
stanzheng <dev.stanzheng@gmail.com>
2014-03-06 04:31:45 -0500

    can fucking delete now, that was really dumb i forgot to execute the statement

--- project/delete.php
+++ project/delete.php
@@ -13,14 +13,13 @@ include 'include/connect_database.php';
 
     $tbl_name = "reply";
     $query="DELETE FROM reply WHERE reply_id={$_GET['reply_id']}";
-    //var_dump($query);
-    //die();
     $result=mysql_query($query);
     }
-    else if(isset($_GET['id'])){
+    else if(isset($_GET['thread_id'])){
 
-    $tbl_name = "thread_id";
+    $tbl_name = "thread";
     $query="DELETE FROM thread WHERE thread_id={$_GET['thread_id']}";
+    $result=mysql_query($query);
   }
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 header('refresh:5;url= ' . $_SERVER['HTTP_REFERER']);
