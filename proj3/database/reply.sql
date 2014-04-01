6052b4566b7586ced544ecec4d9a230218032990
stanzheng <dev.stanzheng@gmail.com>
2014-04-01 02:03:11 -0400

    updated schema

--- proj3/database/reply.sql
+++ proj3/database/reply.sql
@@ -6,5 +6,10 @@ CREATE TABLE reply (reply_id INT(5) NOT NULL AUTO_INCREMENT,
               reply_title VARCHAR(75) NOT NULL,
               reply_post  VARCHAR(1000) NOT NULL,
               reply_date DATETIME NOT NULL,
+              reply_frozen BOOLEAN NOT NULL,
+              reply_editby VARCHAR(30) NOT NULL,
+              reply_edited BOOLEAN NOT NULL,
               PRIMARY KEY (reply_id));
-INSERT INTO reply (reply_id,reply_user,reply_thread_id,reply_reply_id,reply_reply_to_id ,reply_title,reply_post,reply_date) values (null,'TEST3',1, 0,-1, 'test2 title','NOTHING LIKE THE REST ', NOW());
+INSERT INTO reply (reply_id,reply_user,reply_thread_id,reply_reply_id,reply_reply_to_id ,reply_title,reply_post,reply_date,reply_frozen,reply_editby,reply_edited) values (null,'TEST3',1, 0,-1, 'test2 title','NOTHING LIKE THE REST ', NOW(), FALSE, "admin",FALSE);
+#is frozen property
+#suspend
--- proj3/database/user.sql
+++ proj3/database/user.sql
@@ -4,17 +4,18 @@ user_name   	VARCHAR(30) NOT NULL,
 user_password   VARCHAR(255) NOT NULL,
 user_email  	VARCHAR(255) NOT NULL,
 user_date   	DATETIME NOT NULL,
+user_suspended  BOOLEAN NOT NULL,
 UNIQUE INDEX 	user_name_unique (user_name),
 PRIMARY KEY 	(user_id)
 );
 
 //insert
-INSERT into users (user_id, user_name,user_password,user_email,user_date) values (null,"admin","admin","admin@cs.odu.edu",NOW());
-INSERT into users (user_id, user_name,user_password,user_email,user_date) values (null,"user","user","user@cs.odu.edu",NOW());
-INSERT into users (user_id, user_name,user_password,user_email,user_date) values (null,"szheng","szheng","szheng@cs.odu.edu",NOW());
-INSERT into users (user_id, user_name,user_password,user_email,user_date) values (null,"test","test","test",NOW());
-INSERT into users (user_id, user_name,user_password,user_email,user_date) values (null,"test2","test2","test2",NOW());
-INSERT into users (user_id, user_name,user_password,user_email,user_date) values (null,"root","root","root@root.com",NOW());
-INSERT into users (user_id, user_name,user_password,user_email,user_date) values (null,"mweigle","mweigle","mweigle@cs.odu.edu",NOW());
-INSERT into users (user_id, user_name,user_password,user_email,user_date) values (null,"TA","TA","TA@cs.odu.edu",NOW());
-INSERT into users (user_id, user_name,user_password,user_email,user_date) values (null,"orattana","orattana","orattana@cs.odu.edu",NOW());
+INSERT into users (user_id, user_name,user_password,user_email,user_date,user_suspended) values (null,"admin","admin","admin@cs.odu.edu",NOW(),FALSE);
+INSERT into users (user_id, user_name,user_password,user_email,user_date,user_suspended) values (null,"user","user","user@cs.odu.edu",NOW(),FALSE);
+INSERT into users (user_id, user_name,user_password,user_email,user_date,user_suspended) values (null,"szheng","szheng","szheng@cs.odu.edu",NOW(),FALSE);
+INSERT into users (user_id, user_name,user_password,user_email,user_date,user_suspended) values (null,"test","test","test",NOW(),FALSE);
+INSERT into users (user_id, user_name,user_password,user_email,user_date,user_suspended) values (null,"test2","test2","test2",NOW(),FALSE);
+INSERT into users (user_id, user_name,user_password,user_email,user_date,user_suspended) values (null,"root","root","root@root.com",NOW(),FALSE);
+INSERT into users (user_id, user_name,user_password,user_email,user_date,user_suspended) values (null,"mweigle","mweigle","mweigle@cs.odu.edu",NOW(),FALSE);
+INSERT into users (user_id, user_name,user_password,user_email,user_date,user_suspended) values (null,"TA","TA","TA@cs.odu.edu",NOW(),FALSE);
+INSERT into users (user_id, user_name,user_password,user_email,user_date,user_suspended) values (null,"orattana","orattana","orattana@cs.odu.edu",NOW(),FALSE);
