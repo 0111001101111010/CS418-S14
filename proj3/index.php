35551996dca79aad91cb580ca008d4fe83f2b2cd
Lookmai Rattana <oratt001@gmail.com>
2014-04-01 23:15:32 -0400

    found the thing on index and removed it

--- proj3/index.php
+++ proj3/index.php
@@ -6,47 +6,48 @@
   <div class="container">
 
 <?php
-# Perform database query
-$query = "SELECT * from board";
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+  # Perform database query
+  $query = "SELECT * from board";
+  $result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
-//$stmt = $conn->prepare('SELECT * from thread');
-//$stmt->execute();
+  //$stmt = $conn->prepare('SELECT * from thread');
+  //$stmt->execute();
 
-# Filter through rows and echo desired information
-if ($result){
-  while ($row = mysql_fetch_object($result)) {
-
-    echo $thread=<<<EOD
+  # Filter through rows and echo desired information
+  if ($result){
+    while ($row = mysql_fetch_object($result)) {
+      echo $thread=<<<EOD
         <div class="board">
           <h2><a href="forum.php?&board_id=$row->board_id">$row->board_title</a></h2>
           <p>$row->board_description<p>
           <div class="info"><b>Moderators</b>
 EOD;
-//print the moderators
-$query2 = "SELECT * from moderator where moderator_board_id=". $row->board_id." and moderator_user_level=5";//.$row->board_id;
-$result2 = mysql_query($query2) or die('Query failed: ' . mysql_error());
 
+      //print the moderators
+      $query2 = "SELECT * from moderator where moderator_board_id=". $row->board_id." and moderator_user_level=5";
+      //.$row->board_id;
+      $result2 = mysql_query($query2) or die('Query failed: ' . mysql_error());
 
-while ($record = mysql_fetch_object($result2)) {
-echo " ". $record->moderator_name_id." ";
-}
-//if you're an admin
-if($_COOKIE['user']=='admin')
-echo '<div class="setting pull-right"><a href=""><i class="fa fa-pencil"></i></a> <a href=delete.php?board_id="'.$row->board_id.'"><i class="fa fa-times"></i></a></div>';
+      while ($record = mysql_fetch_object($result2)) {
+        echo " " .$record->moderator_name_id. " ";
+      }
 
-echo "</div></div>'";
-}
-}
-else {
-    echo "crud";
-}
+      //if you're an admin
+      if($_COOKIE['user']=='admin')
+        echo '<div class="setting pull-right"><a href=""><i class="fa fa-pencil"></i></a> <a href=delete.php?board_id="'.$row->board_id.'"><i class="fa fa-times"></i></a></div>';
 
+      echo "</div></div>";
+    } 
 
-if ($_COOKIE['user']=='admin'){
- echo '<button class="btn btn-blue pull-right" data-toggle="modal" data-target="#newboard" >Create Board <i class="fa fa-plus-square"></i></button>
-';
-}
+  }
+
+  else {
+      echo "crud";
+  }
+
+  if ($_COOKIE['user']=='admin'){
+   echo '<button class="btn btn-blue pull-right" data-toggle="modal" data-target="#newboard" >Create Board <i class="fa fa-plus-square"></i></button>';
+  }
 
 ?>
   <!-- Modal -->
