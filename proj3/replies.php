33d136c27539f71cf16fa19c9e4cb01689571b7a
stanzheng <dev.stanzheng@gmail.com>
2014-04-03 00:13:08 -0400

    just need to echo this poo now

--- proj3/replies.php
+++ proj3/replies.php
@@ -138,6 +138,15 @@ if ($result){
     if ($row!=null){
 if ($_GET['edit']==$row['reply_id']){
   echo "foo";
+  echo '<form method="post" action="insert.php?replyto='.urldecode($_GET[thread]).'&id='.$_GET[id].'" class="response">
+          <h3>Reply title: </h3>
+            <input type="text" name="reply_title">
+
+          <h3>Comment: </h3>
+            <textarea name="reply"></textarea><br><br>
+            <input type="submit" name="Submit" value="Submit" class="btn btn-blue pull-right">
+
+        </form>';
 }
 else{
 /**  row threads**/
@@ -263,7 +272,7 @@ echo $js;
     $rowX = mysql_fetch_array($resultX);
     $frozen = $rowX["thread_frozen"];
 
-if (isset($_COOKIE['user'])&& ($frozen==true)){
+if (isset($_COOKIE['user'])&& ($frozen==true) && !$_GET['edit']){
   echo '<hr>
         <form method="post" action="insert.php?replyto='.urldecode($_GET[thread]).'&id='.$_GET[id].'" class="response">
           <h3>Reply title: </h3>
