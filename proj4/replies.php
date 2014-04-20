907df65450865a676bec5c95b3e3f96080bfa7e2
stanzheng <dev.stanzheng@gmail.com>
2014-04-20 13:18:22 -0400

    fixing up indentations

--- proj4/replies.php
+++ proj4/replies.php
@@ -4,55 +4,55 @@
 <?php
 session_start();
 
-//get settings for pagination first
-$query5 = "SELECT * from settings";
-$result5 = mysql_query($query5) or die('Query failed: ' . mysql_error());
-$row5 = mysql_fetch_array($result5);
-$page_setting = $row5["setting_value"];
+  //get settings for pagination first
+  $query5 = "SELECT * from settings";
+  $result5 = mysql_query($query5) or die('Query failed: ' . mysql_error());
+  $row5 = mysql_fetch_array($result5);
+  $page_setting = $row5["setting_value"];
 
-//calculate num pages;
-//to and from
-$start = $_GET[page]*$page_setting;
-$end   = $page_setting;
+  //calculate num pages;
+  //to and from
+  $start = $_GET[page]*$page_setting;
+  $end   = $page_setting;
 
-$query = "SELECT * from reply where reply_thread_id =".$_REQUEST['id']." limit ". $start.','.$end;
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-//var_dump($query);
-//die();
-$queryCount = "SELECT * from reply where reply_thread_id =".$_REQUEST['id'];
-$resultCount = mysql_query($queryCount) or die('Query failed: ' . mysql_error());
-$count = mysql_num_rows($resultCount);
+  $query = "SELECT * from reply where reply_thread_id =".$_REQUEST['id']." limit ". $start.','.$end;
+  $result = mysql_query($query) or die('Query failed: ' . mysql_error());
+  //var_dump($query);
+  //die();
+  $queryCount = "SELECT * from reply where reply_thread_id =".$_REQUEST['id'];
+  $resultCount = mysql_query($queryCount) or die('Query failed: ' . mysql_error());
+  $count = mysql_num_rows($resultCount);
 
-$query2 = "SELECT * from thread where thread_id =".$_REQUEST['id'];
-$result2 = mysql_query($query2) or die('Query failed: ' . mysql_error());
-//get id agains
-$query3 = "SELECT * from thread where thread_id =".$_REQUEST['id'];
-$result3 = mysql_query($query3) or die('Query failed: ' . mysql_error());
-//get thread ID
-$query4 = "SELECT * from thread where thread_id =".$_REQUEST['id'];
-$result4 = mysql_query($query4) or die('Query failed: ' . mysql_error());
+  $query2 = "SELECT * from thread where thread_id =".$_REQUEST['id'];
+  $result2 = mysql_query($query2) or die('Query failed: ' . mysql_error());
+  //get id agains
+  $query3 = "SELECT * from thread where thread_id =".$_REQUEST['id'];
+  $result3 = mysql_query($query3) or die('Query failed: ' . mysql_error());
+  //get thread ID
+  $query4 = "SELECT * from thread where thread_id =".$_REQUEST['id'];
+  $result4 = mysql_query($query4) or die('Query failed: ' . mysql_error());
 
-$query5 = "SELECT * from board where board_id =".$_GET['id'];
-$result4 = mysql_query($query4) or die('Query failed: ' . mysql_error());
-// var_dump($row5);
-// die();
-$user = $_COOKIE['user'];
+  $query5 = "SELECT * from board where board_id =".$_GET['id'];
+  $result4 = mysql_query($query4) or die('Query failed: ' . mysql_error());
+  // var_dump($row5);
+  // die();
+  $user = $_COOKIE['user'];
 ?>
 
-  <div class="content">
-    <div class="container">
+<div class="content">
+  <div class="container">
 
 <?php
-var_dump($_GET[thread]);
-$returnResult4 = mysql_fetch_object($result4);
-//var_dump(mysql_fetch_object($result3)->thread_description);
+  var_dump($_GET[thread]);
+  $returnResult4 = mysql_fetch_object($result4);
+  //var_dump(mysql_fetch_object($result3)->thread_description);
 
-//mysql_fetch_object($result3)->thread_description
-$title = urldecode(mysql_fetch_object($result3)->thread_description);
-$whichBoard = $returnResult4->thread_board_id;
-$theThread=$_GET["id"];
-$urltitle= urlencode($_GET[thread]);
-echo $breadcrumb= <<< EOD
+  //mysql_fetch_object($result3)->thread_description
+  $title = urldecode(mysql_fetch_object($result3)->thread_description);
+  $whichBoard = $returnResult4->thread_board_id;
+  $theThread=$_GET["id"];
+  $urltitle= urlencode($_GET[thread]);
+  echo $breadcrumb= <<< EOD
 <h2>
   <a href=index.php>HackChat</a>->
   <a href=forum.php?board_id=$returnResult4->thread_board_id>..</a>->
@@ -61,75 +61,67 @@ echo $breadcrumb= <<< EOD
 EOD;
 
 ?>
-      <div class="post">
-        <h3><?php echo $_REQUEST['thread']?></h3>
-        <h6>Original Poster:
-          <?php
-            //count their number of posts
-  //$returnResult4 = mysql_fetch_object($result4);//Removed and moved up
-  $threadUser = $returnResult4->thread_user;
-  $whichBoard = $returnResult4->thread_board_id;
-  // var_dump($returnResult4);
-  //var_dump($threadUser);
- // var_dump($whichBoard);
-  //die();
-  # Perform database count user  boards
-  $queryCount = "select * from reply where reply_user ='".$threadUser."';";
-  $resultCount = mysql_query($queryCount) or die('Query failed: ' . mysql_error());
-  $posts = mysql_num_rows($resultCount);
-  //var_dump($queryCount);
-  //die();
-  /* AM I A MODERATOR OR ADMIN?*/
-    {//encapsulating
-    $queryModerator = "SELECT * from moderator where moderator_name_id=".'"'.$threadUser.'" and moderator_board_id ='.$whichBoard;//.$_GET['id'];
+<div class="post">
+  <h3><?php echo $_REQUEST['thread']?></h3>
+  <h6>Original Poster:
+    <?php
 
-    $resultModerator = mysql_query($queryModerator) or die('Query failed: ' . mysql_error());
-    //$result3 = mysql_fetch_object($result3);
-    //#moderator number
-    //#is an ADMIN search ** this is different
-    $queryAdmin = "SELECT * from users where user_name=".'"'.$threadUser."\";";
-    $resultAdmin = mysql_query($queryAdmin) or die('Query failed: ' . mysql_error());
-    $admin = mysql_fetch_object($resultAdmin)->user_admin;
+    $threadUser = $returnResult4->thread_user;
+    $whichBoard = $returnResult4->thread_board_id;
 
-    $user_level_array =mysql_fetch_object($resultModerator);
-    $permission=mysql_num_rows($resultModerator);
+    # Perform database count user  boards
+    $queryCount = "select * from reply where reply_user ='".$threadUser."';";
+    $resultCount = mysql_query($queryCount) or die('Query failed: ' . mysql_error());
+    $posts = mysql_num_rows($resultCount);
+
+/* AM I A MODERATOR OR ADMIN?*/
+  {//encapsulating
+  $queryModerator = "SELECT * from moderator where moderator_name_id=".'"'.$threadUser.'" and moderator_board_id ='.$whichBoard;//.$_GET['id'];
+
+  $resultModerator = mysql_query($queryModerator) or die('Query failed: ' . mysql_error());
+  //$result3 = mysql_fetch_object($result3);
+  //#moderator number
+  //#is an ADMIN search ** this is different
+  $queryAdmin = "SELECT * from users where user_name=".'"'.$threadUser."\";";
+  $resultAdmin = mysql_query($queryAdmin) or die('Query failed: ' . mysql_error());
+  $admin = mysql_fetch_object($resultAdmin)->user_admin;
+
+  $user_level_array =mysql_fetch_object($resultModerator);
+  $permission=mysql_num_rows($resultModerator);
+  }
+$moderator=$user_level_array->moderator_user_level;
+
+
+  // // if not admin or mod
+  // // if admin -- star
+  if ($admin==true){
+    echo '<i class="fa fa-star"></i> ';
+  }
+  // // if moderator -- bookmark
+  else if ($moderator==true){
+     echo '<i class="fa fa-bookmark"></i> ';
+  }
+  else {
+    // if # of post is less than 10 -- user
+   if ($posts >0 && $posts <10){
+      echo '<i class="fa fa-user"></i> ';
+   }
+    // if # of post is between 10 and 50 -- coffee
+    else if($posts>10 && $posts<50){
+      echo '<i class="fa fa-coffee"></i> ';
     }
-    //var_dump($user_level_array->moderator_user_level);
-    $moderator=$user_level_array->moderator_user_level;
-    //var_dump($queryModerator);
-  //  var_dump($moderator);
-   // die();
+    // if # of post is greater than 50 -- check
+    else if($posts>50){
+      echo '<i class="fa fa-check-circle"></i> ';
+    }
+  }
+ echo $threadUser?> </a></h6><h6>
+ <?php echo mysql_fetch_object($result2)->thread_date;?></h6>
+    <p><?echo mysql_fetch_object($result3)->thread_description;
+    ?> </p>
+  </div>
 
-            // // if not admin or mod
-            // // if admin -- star
-            if ($admin==true){
-              echo '<i class="fa fa-star"></i> ';
-            }
-            // // if moderator -- bookmark
-            else if ($moderator==true){
-               echo '<i class="fa fa-bookmark"></i> ';
-            }
-            else {
-              // if # of post is less than 10 -- user
-             if ($posts >0 && $posts <10){
-                echo '<i class="fa fa-user"></i> ';
-             }
-              // if # of post is between 10 and 50 -- coffee
-              else if($posts>10 && $posts<50){
-                echo '<i class="fa fa-coffee"></i> ';
-              }
-              // if # of post is greater than 50 -- check
-              else if($posts>50){
-                echo '<i class="fa fa-check-circle"></i> ';
-              }
-            }
-     echo $threadUser?> </a></h6><h6>
-     <?php echo mysql_fetch_object($result2)->thread_date;?></h6>
-        <p><?echo mysql_fetch_object($result3)->thread_description;
-        ?> </p>
-      </div>
-
-      <hr>
+  <hr>
 
 
 
@@ -139,12 +131,6 @@ EOD;
 //die();
 $url = "replies.php?thread=".urldecode($_GET[thread]).'&id='.$_GET[id];
 
-//var_dump(mysql_fetch_object($result));
-//die();
-//if it has been specified reset these points
-/*
-#TODO some crazy ass shit
-*/
 if (isset($_GET['page'])){
   $start = 0+$_GET['page']*$page_setting;
   $end = $page_setting+ $_GET['page']*$page_setting;
@@ -185,47 +171,43 @@ else{
 
     $user_level_array =mysql_fetch_object($resultModerator);
     $permission=mysql_num_rows($resultModerator);
-   //var_dump($user_level_array);
-        // if($admin==1) //does it exist
-        //     $moderator=true;
-        //     echo '<i class="fa fa-star"></i> ';
     }
-            // // if not admin or mod
-            // // if admin -- star
-            if ($admin==true){
-              $icon = '<i class="fa fa-star"></i> ';
-            }
-            // // if moderator -- bookmark
-            else if ($permission==true){
-               $icon ='<i class="fa fa-bookmark"></i>';
-            }
-            else {
-              // if # of post is less than 10 -- user
-              if ($posts >0 && $posts <10){
-                $icon ='<i class="fa fa-user"></i> ';
-              }
-              // if # of post is between 10 and 50 -- coffee
-              else if($posts>10 && $posts<50){
-                $icon ='<i class="fa fa-coffee"></i> ';
-              }
-              // if # of post is greater than 50 -- check
-              else if($posts>50){
-                $icon ='<i class="fa fa-check-circle"></i> ';
-              }
-            }
+// // if not admin or mod
+// // if admin -- star
+if ($admin==true){
+  $icon = '<i class="fa fa-star"></i> ';
+}
+// // if moderator -- bookmark
+else if ($permission==true){
+   $icon ='<i class="fa fa-bookmark"></i>';
+}
+else {
+  // if # of post is less than 10 -- user
+  if ($posts >0 && $posts <10){
+    $icon ='<i class="fa fa-user"></i> ';
+  }
+  // if # of post is between 10 and 50 -- coffee
+  else if($posts>10 && $posts<50){
+    $icon ='<i class="fa fa-coffee"></i> ';
+  }
+  // if # of post is greater than 50 -- check
+  else if($posts>50){
+    $icon ='<i class="fa fa-check-circle"></i> ';
+  }
+}
 
 
-          $id = $row['reply_id'];
-          echo '<div class="post">
-            <h3>'.$row["reply_title"].'</h3>
-            <h6>User: <a href="">'.$icon.$row["reply_user"].'</a>
-            </h6>
-            <h6>Posted on '. date("\n l F jS Y @\t\t g:ia",strtotime("-45 minutes",strtotime($row["reply_date"]))) .'</h6>
+$id = $row['reply_id'];
+    echo '<div class="post">
+    <h3>'.$row["reply_title"].'</h3>
+      <h6>User: <a href="">'.$icon.$row["reply_user"].'</a>
+      </h6>
+    <h6>Posted on '. date("\n l F jS Y @\t\t g:ia",strtotime("-45 minutes",strtotime($row["reply_date"]))) .'</h6>
         <p>'.$row["reply_post"].'</p>
         <br>';
-        if ($row['reply_edited']==true){
-            echo "edited by ". $row['reply_editby'];
-        }
+    if ($row['reply_edited']==true){
+    echo "edited by ". $row['reply_editby'];
+    }
   /* IS VIEWER*/
     {//encapsulating
     $viewUser = $_COOKIE["user"];
@@ -245,10 +227,6 @@ else{
     // check if person is logged in
         if(isset($_COOKIE["user"])){
           $user = $_COOKIE["user"];
-          // var_dump($moderator);
-          // var_dump($admin);
-          // var_dump($user==$row['reply_user'] );
-          // var_dump($user==$row['reply_user'] || $moderator || $admin );
           if ($viewUser==$row['reply_user'] || $moderator || $admin){
               echo $buttons = <<<EOD
               <div class="pull-right" style="font-size:20px; margin-top:-20px;">
