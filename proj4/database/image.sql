0ac852cc350d056a53421e6c9024eaa12d5a1044
stanzheng <dev.stanzheng@gmail.com>
2014-04-27 00:03:33 -0400

    save image username

--- proj4/database/image.sql
+++ proj4/database/image.sql
@@ -1,6 +1,6 @@
 CREATE TABLE image (
 image_id INT(5) NOT NULL AUTO_INCREMENT,
-auxon    VARCHAR(30) NOT NULL,
+user    VARCHAR(30) NOT NULL,
 path   	VARCHAR(30) NOT NULL,
 PRIMARY KEY 	(image_id)
 );
