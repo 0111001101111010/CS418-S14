b2e298cfa46b20d785b5984c6ed6a40885030401
stanzheng <dev.stanzheng@gmail.com>
2014-06-30 23:15:46 -0400

    upload pictures

--- proj4/profile.php
+++ proj4/profile.php
@@ -11,6 +11,21 @@ $_SESSION['authuser'] = 0;
 <?php include 'include/connect_database.php';?>
 <?
   $user = $_GET['user'];
+
+  if(!empty($_POST)){ // Checks if the form is submitted or not
+     unlink("uploads/".$user.".jpg");
+     $ext = pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION);
+     $file="uploads/".$user.'.'.$ext;//.$_FILES["image"]["name"];
+       move_uploaded_file($_FILES["image"]["tmp_name"],$file);
+       echo"<font size = '5'><font color=\"#0CF44A\">SAVED<br></font>";
+       //extention
+       $sql="INSERT INTO image (image_id, user, path) VALUES (null,'$username','$file')" or die('Error: ' . mysql_error());;
+       $img = create_cropped_thumbnail($file, 200, 200,'');
+       if (!mysql_query($sql))
+       {
+          die('Error: ' . mysql_error());
+        }
+  }
 ?>
 
     </head>
@@ -20,14 +35,24 @@ $_SESSION['authuser'] = 0;
       <div class="content">
         <div class="container">
           <div class="board">
-            <h3 style="display:inline">cosmicmeow</h3> >> <a href="registration.html"><h4 style="display:inline">Edit Profile</h4></a>
+            <h3 style="display:inline"><?php echo $user; ?></h3> <a href="registration.html"><h4 style="display:inline">Edit Profile</h4></a>
             <hr>
             <div class="userbasic" style="padding:20px;background-color:#f4f4f4;margin-bottom:20px">
-              <img src="uploads/<?php echo $user;?>"style="border:3px solid #5CCDCC;vertical-align:top">
+              <img src="uploads/<?php echo $user?>"style="border:3px solid #5CCDCC;vertical-align:top">
               <div class="info" style="display:inline-block;margin-left:10px">
-                <h3 style="margin:0"><i class="fa fa-bookmark"></i> cosmicmeow</h3>
+                <h3 style="margin:0"></i> <?php echo $user ?></h3>
                 <h6 style="margin-top:0">Joined on Feb 3, 2014 | Total Posts: 15</h6>
-                <p style="width:400px">My name is Lookmai. I'm a co-creator of this forum as well as moderator of some of the boards. :D Hi!</p>
+                <?php
+if($_COOKIE["user"]==$user){
+echo $breadcrumb = <<< EOD
+<form method="POST" class="form-horizontal" role="form" action="profile.php?&user=$user"  enctype="multipart/form-data">
+  <label name="Profile picture" value = "profile"> profile picture, JPG, GIF, or PNG only please. </label>
+  <input name="image" type="file" id="image" accept="image/gif,image/jpeg,image/png">
+  <button type="submit" class="btn">Submit</button>
+</form>
+EOD;
+}
+?>
               </div>
             </div>
 
@@ -39,41 +64,6 @@ $_SESSION['authuser'] = 0;
                 <p>Sounds like a great idea!</p>
               </div>
               <div style="background-color:#eee;padding:12px;">
-                <a href="">FriendFinder for ODU students</a>
-                <h6 style="margin-top:0">Original Posts: <a href="">szheng</a> | Posted on April 1, 2014 | Board: <a href="">Cool Ideas</a></h6>
-                <p>Sounds like a great idea!</p>
-              </div>
-              <div style="background-color:#fff;padding:12px;">
-                <a href="">FriendFinder for ODU students</a>
-                <h6 style="margin-top:0">Original Posts: <a href="">szheng</a> | Posted on April 1, 2014 | Board: <a href="">Cool Ideas</a></h6>
-                <p>Sounds like a great idea!</p>
-              </div>
-              <div style="background-color:#eee;padding:12px;">
-                <a href="">FriendFinder for ODU students</a>
-                <h6 style="margin-top:0">Original Posts: <a href="">szheng</a> | Posted on April 1, 2014 | Board: <a href="">Cool Ideas</a></h6>
-                <p>Sounds like a great idea!</p>
-              </div>
-              <div style="background-color:#fff;padding:12px;">
-                <a href="">FriendFinder for ODU students</a>
-                <h6 style="margin-top:0">Original Posts: <a href="">szheng</a> | Posted on April 1, 2014 | Board: <a href="">Cool Ideas</a></h6>
-                <p>Sounds like a great idea!</p>
-              </div>
-              <div style="background-color:#eee;padding:12px;">
-                <a href="">FriendFinder for ODU students</a>
-                <h6 style="margin-top:0">Original Posts: <a href="">szheng</a> | Posted on April 1, 2014 | Board: <a href="">Cool Ideas</a></h6>
-                <p>Sounds like a great idea!</p>
-              </div>
-              <div style="background-color:#fff;padding:12px;">
-                <a href="">FriendFinder for ODU students</a>
-                <h6 style="margin-top:0">Original Posts: <a href="">szheng</a> | Posted on April 1, 2014 | Board: <a href="">Cool Ideas</a></h6>
-                <p>Sounds like a great idea!</p>
-              </div>
-              <div style="background-color:#eee;padding:12px;">
-                <a href="">FriendFinder for ODU students</a>
-                <h6 style="margin-top:0">Original Posts: <a href="">szheng</a> | Posted on April 1, 2014 | Board: <a href="">Cool Ideas</a></h6>
-                <p>Sounds like a great idea!</p>
-              </div>
-              <div style="background-color:#fff;padding:12px;">
                 <a href="">FriendFinder for ODU students</a>
                 <h6 style="margin-top:0">Original Posts: <a href="">szheng</a> | Posted on April 1, 2014 | Board: <a href="">Cool Ideas</a></h6>
                 <p>Sounds like a great idea!</p>
