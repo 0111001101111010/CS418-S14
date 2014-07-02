868d640d0a7b263ff59f87583bb440a22649ac00
stanzheng <dev.stanzheng@gmail.com>
2014-07-01 20:16:09 -0400

    the add works properly

--- proj4/util/check.php
+++ proj4/util/check.php
@@ -1,5 +1,5 @@
 <?php
-
+ob_start();
 require 'CaptchasDotNet.php';
 
 // See query.php for documentation
@@ -15,11 +15,6 @@ $password      = $_REQUEST['password'];
 $random_string = $_REQUEST['random'];
 ?>
 
-<html>
-  <head>
-    <title>Sample PHP CAPTCHA Query</title>
-  </head>
-  <h1>Sample PHP CAPTCHA Query</h1>
 
 <?php
   // Check the random string to be valid and return an error message
@@ -30,18 +25,23 @@ $random_string = $_REQUEST['random'];
     echo 'In case you are the administrator of this page, ';
     echo 'please check if random keys are stored correct.<br/>';
     echo 'See http://captchas.net/sample/php/ "Problems with save mode"';
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    header('refresh:5;url= ' . $_SERVER['HTTP_REFERER']);
+    ob_flush();
   }
   // Check, that the right CAPTCHA password has been entered and
   // return an error message otherwise.
   elseif (!$captchas->verify ($password))
   {
-    echo 'You entered the wrong password. Aren\'t you human? Please use back button and reload.';
+    echo 'You entered the wrong password. Aren\'t you human? Sending you back!';
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    header('refresh:10;url= ' . $_SERVER['HTTP_REFERER']);
+    ob_flush();
   }
   // Return a success message
   else
   {
     echo 'Your message was verified to be entered by a human and is "' . $message . '"';
+    //encapsualte the rest of the page here
   }
 ?>
-
-</html>
