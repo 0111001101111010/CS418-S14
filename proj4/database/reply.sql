2797b79b40599f8dcdff2065b0ead9e28c8fd349
stanley zheng <dev.stanzheng@gmail.com>
2014-07-20 22:15:39 -0400

    adding schema for text search of title

--- proj4/database/reply.sql
+++ proj4/database/reply.sql
@@ -12,6 +12,5 @@ CREATE TABLE reply (reply_id INT(5) NOT NULL AUTO_INCREMENT,
               reply_board_id INT(5) NOT NULL,
               PRIMARY KEY (reply_id));
 INSERT INTO reply (reply_id,reply_user,reply_thread_id,reply_reply_id,reply_reply_to_id ,reply_title,reply_post,reply_date,reply_frozen,reply_editby,reply_edited,reply_board_id) values (null,'TEST3',1, 0,-1, 'test2 title','NOTHING LIKE THE REST ', NOW(), FALSE, "admin",FALSE, 1);
-INSERT INTO reply (reply_id,reply_user,reply_thread_id,reply_reply_id,reply_reply_to_id ,reply_title,reply_post,reply_date,reply_frozen,reply_editby,reply_edited,reply_board_id) values (null,'admin',11, 15,-1, 'test2 title','NOTHING LIKE THE REST ', NOW(), FALSE, "admin",true, 1);
 #is frozen property
 #suspend
--- proj4/database/thread.sql
+++ proj4/database/thread.sql
@@ -6,7 +6,9 @@ thread_name VARCHAR(75) NOT NULL,
 thread_frozen BOOLEAN NOT NULL,
 thread_description VARCHAR(100) NOT NULL,
 thread_date DATETIME NOT NULL,
-PRIMARY KEY (thread_id));
+PRIMARY KEY (thread_id),
+FULLTEXT (thread_name)
+);
 
 INSERT INTO thread (thread_id, thread_board_id, thread_user, thread_name,thread_description, thread_date)
 values (null, 1, "admin","Frozen Quote Machine", "Do you like to build a snowman", NOW());
